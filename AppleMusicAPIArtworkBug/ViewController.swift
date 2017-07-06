016a89246da614e3002e684e88ba57cf5c0f574f
carpentryplus25 <williamt1984@yahoo.com>
2017-07-06 13:53:05 -0400

    Initial Commit

---
+++ AppleMusicAPIArtworkBug/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AppleMusicAPIArtworkBug
+//
+//  Created by Thompson on 7/6/17.
+//  Copyright © 2017 J.W.Enterprises LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
